77f5904f1c72eee4944db66859c0d2f0237c91c6
niravsatva <niravp19697@gmail.com>
2023-10-09 11:52:15 +0530

    sprint-4

--- app/repositories/payPeriodRepository.js
+++ app/repositories/payPeriodRepository.js
@@ -19,7 +19,7 @@ class PayPeriodRepository {
                 skip: offset,
                 take: limit,
                 orderBy: {
-                    startDate: 'desc',
+                    startDate: 'asc',
                 },
             };
             if (!offset) {
